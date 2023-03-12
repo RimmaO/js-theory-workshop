770e7b8f94de984030ef619c89cde87ffd78043a
RimmaO <rimma_o@ukr.net>
2023-03-12 20:57:04 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Example 1 - Введення користувача та розгалуження
 // Використовуючи конструкцію if..else та prompt, напиши код, який питатиме: "Яка офіційна назва JavaScript?". Якщо користувач вводить ECMAScript, то показуй alert з рядком "Правильно!", в іншому випадку - "Не знаєте? ECMAScript!"
+
 // const nameJavaScript = prompt("Яка офіційна назва JavaScript?");
 
 // if (nameJavaScript === "ECMAScript") {
@@ -7,10 +8,12 @@
 // } else {
 //   alert("Не знаєте? ECMAScript!");
 // }
+
 // console.log(nameJavaScript);
 
 // Example 2 - Відображення часу (if...else)
 // Напиши скрипт для відображення годин та хвилин у консолі браузера у вигляді рядка формату "14 г. 26 хв.". Якщо значення змінної minutes дорівнює 0, то виводь рядок "14 г.", без хвилин.
+
 // const hours = 14;
 // const minutes = 26;
 // let timestring;
@@ -25,6 +28,7 @@
 
 // Example 3 - Розгалуження
 // Напиши скрипт, який виводить у консоль рядок "Це позитивне число", якщо у prompt користувач ввів число більше нуля. Якщо було введено нуль, виводь в консоль рядок "Це нуль". Якщо передали від'ємне число, у консолі повинен бути рядок "Це негативне число".
+
 // const userInput = prompt("Введіть число");
 
 // if (userInput > 0) {
@@ -37,13 +41,15 @@
 //   alert("Це не число");
 // }
 
-// console.log(userInput);
+// console.log(typeof userInput);
 // +userInput=Number(userInput)
 
 // Example 4 - Вкладені розгалуження
 // Напиши скрипт, який порівнює числа у змінних a та b. Якщо обидва значення більше 100, то виведи в консоль максимальне з них. В протилежному випадку у консолі повинна бути сума значення b та числа 512.
+
 // const a = 120;
 // const b = 180;
+
 // if (a > 100 && b > 100) {
 //   console.log(Math.max(a, b));
 // } else {
@@ -52,18 +58,23 @@
 
 // Example 5 - Форматування посилання (endsWith)
 // Напиши скрипт який перевіряє чи закінчується значення змінної link символом /. Якщо ні, додай до кінця значення link цей символ. Використовуй конструкцію if...else.
+
 // let link = "https://my-site.com/about";
 // let userLink;
+
 // if (link.endsWith("/")) {
 //   userLink = link;
 // } else {
 //   userLink = link + "/";
 // }
+
 // console.log(userLink);
 
 // Example 6 - Форматування посилання (includes та логічне «І»)
 // Напиши скрипт який перевіряє чи закінчується значення змінної link символом /. Якщо ні, додай до кінця значення link цей символ, але тільки в тому випадку, якщо в link є підрядок "my-site". Використовуй конструкцію if...else.
+
 // let link = "https://somesite.com/about";
+
 // if (link.endsWith("/")) {
 //   link = link;
 // } else if (link.includes("my-site")) {
@@ -71,17 +82,19 @@
 // } else {
 //   link = link;
 // }
+
 // console.log(link);
 
-// Example 7 - Форматування посилання (тернарний оператор)
-// Виконай рефакторинг коду задачі номер 4, використовуючи тернарний оператор.
 // let link = 'https://somesite.com/about';
 // if (link.includes('my-site') && !link.endsWith('/')) {
 //   link += '/';
 // }
 // console.log(link);
 
-// Задача 4
+// Example 7 - Форматування посилання (тернарний оператор)
+// Виконай рефакторинг коду задачі номер 4, використовуючи тернарний оператор.
+
+// задача 4
 // const a = 190;
 // const b = 180;
 // if (a > 100 && b > 100) {
@@ -89,7 +102,7 @@
 // } else {
 //   console.log(b + 512);
 // }
-// рефакторинг коду задачі номер 4
+// задача 4 рефакторинг коду
 // const a = 190;
 // const b = 180;
 // const result = a > 100 && b > 100 ? Math.max(a, b) : b + 512;
@@ -114,13 +127,14 @@
 
 // Example 9 - Дедлайн здачі проекту (if...else)
 // Напиши скрипт для відображення часу дедлайну здачі проекту. Використовуй конструкцію if...else.
-
 // Якщо до дедлайну 0 днів - виведи рядок "Сьогодні"
 // Якщо до дедлайну 1 день - виведи рядок "Завтра"
 // Якщо до дедлайну 2 дні - виведи рядок "Післязавтра"
 // Якщо до дедлайну 3+ днів - виведи рядок "Дата у майбутньому"
+
 // const daysUntilDeadline = 5;
 // let deadline;
+
 // if (daysUntilDeadline === 0) {
 //   deadline = "Сьогодні";
 // } else if (daysUntilDeadline === 1) {
@@ -130,12 +144,13 @@
 // } else {
 //   deadline = "Дата у майбутньому";
 // }
+
 // console.log(deadline);
 
 // Example 10 - Дедлайн здачі проекту (switch)
 // Виконай рефакторинг коду задачі номер 5 використовуючи switch.
-// Задача 5 рефакторинг
-// let link = "https://my-site.com/about/;ljhm/bnbmbk/";
+// задача 5 рефакторинг
+// let link = "https://my-site.com/about./";
 // let userLink;
 // switch (link) {
 //   case link.endsWith("/"):
@@ -146,7 +161,8 @@
 //     userLink = link + "/";
 // }
 // console.log(userLink);
-// Задача 9 рефакторинг
+
+// задача 9 рефакторинг
 // const daysUntilDeadline = 2;
 // let deadline;
 // switch (daysUntilDeadline) {
@@ -184,19 +200,29 @@
 // Якщо введено пароль "Я адмін", то вивести рядок "Здрастуйте!"
 // Інакше виводити рядок "Невірний пароль"
 
-const password = "Я адмін";
-let userPassword;
+// const password = "Я адмін";
+// let userPassword;
 // do {
 //   userPassword = prompt("Ввести пароль");
 //   console.log(userPassword);
 // } while (userPassword !== password);
+
 // let text;
 // let userInput;
+
+// const password = "Я адмін";
+
 // if (text === "Адмін") {
-//   userInput = prompt("Ввести пароль");
-// } else if ((text = "" || null)) {
-//   userInput = prompt("Скасовано");
+//   const userInput = prompt("Ввести пароль");
+//   if (userInput === password) {
+//     alert("Здрастуйте!");
+//   } else if (userInput === "" || userInput === null) {
+//     alert("Скасовано");
+//   } else {
+//     alert("Невірний пароль");
+//   }
 // } else {
-//   userInput = prompt("Я вас не знаю");
+//   alert("Я вас не знаю");
 // }
+
 // console.log(userInput);
